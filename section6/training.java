759ea079f4aef9c46ace9891b511d7964b5102d8
Yuto Watanabe <yuto.w51942@gmail.com>
2020-10-28 21:40:52 +0900

    power up

--- section6/training.java
+++ section6/training.java
@@ -35,6 +35,106 @@ class Bank {
   }
 }
 
+class BankOperation {
+  private Bank[] banks = new Bank[1000];
+  private int user;
+
+  BankOperation() {
+    user = 0;
+  }
+
+  void add_user(String name, String number, long balance) {
+    if (user >= 1) {
+      for (int i = 0; user > i; ++i) {
+        if (banks[i].get_number() == number) {
+          throw new IllegalArgumentException("同じ口座番号のユーザーがすでに存在しています。");
+        }
+      }
+    }
+    banks[user] = new Bank(name, number, balance);
+    user++;
+  }
+
+  String[] get_names() {
+    String[] names = new String[user];
+    for (int i = 0; user > i; ++i) {
+      names[i] = banks[i].get_name();
+    }
+
+    return names;
+  }
+
+  String[] get_numbers() {
+    String[] numbers = new String[user];
+    for (int i = 0; user > i; ++i) {
+      numbers[i] = banks[i].get_number();
+    }
+
+    return numbers;
+  }
+
+  long[] get_balances() {
+    long[] balances = new long[user];
+    for (int i = 0; user > i; ++i) {
+      balances[i] = banks[i].get_balance();
+    }
+
+    return balances;
+  }
+
+  String get_name(int index) {
+    check_user(index);
+    return banks[index].get_name();
+  }
+
+  String get_number(int index) {
+    check_user(index);
+    return banks[index].get_number();
+  }
+
+  long get_balance(int index) {
+    check_user(index);
+    return banks[index].get_balance();
+  }
+
+  void deposit(long money, int index) {
+    check_user(index);
+    banks[index].deposit(money);
+  }
+
+  void withdrawal(long money, int index) {
+    check_user(index);
+    banks[index].withdrawal(money);
+  }
+
+  void transaction(String from_number, String send_number, long money) {
+    int from_index = check_number(from_number);
+    int send_index = check_number(send_number);
+
+    withdrawal(money, from_index);
+    deposit(money, send_index);
+  }
+
+  int get_user() {
+    return user;
+  }
+
+  private int check_number(String number) {
+    for (int i = 0; user > i; ++i) {
+      if (banks[i].get_number() == number) {
+        return i;
+      }
+    }
+    throw new IndexOutOfBoundsException("指定されたユーザーは存在しません。");
+  }
+
+  private void check_user(int index) {
+    if (index > (user - 1)) {
+      throw new IndexOutOfBoundsException("指定されたユーザーは存在しません。");
+    }
+  }
+}
+
 public class training {
   public static void main(String[] args) {
     Scanner stdIn = new Scanner(System.in);
@@ -42,7 +142,7 @@ public class training {
     System.out.printf("ユーザー数を入力してください。\n >>> ");
     int length = stdIn.nextInt();
 
-    Bank[] banks = new Bank[length];
+    BankOperation op = new BankOperation();
 
     for (int i = 0; length > i; ++i) {
       System.out.printf("お客様(User%d)の口座情報を入力してください。\n", i);
@@ -54,19 +154,23 @@ public class training {
       long balance = stdIn.nextLong();
       System.out.println("-------------");
 
-      banks[i] = new Bank(name, number, balance);
+      op.add_user(name, number, balance);
     }
 
     // this section is Exercises.
-    banks[0].withdrawal(3000);
-    banks[1].deposit(5000);
+    op.withdrawal(3000, 0);
+    op.deposit(5000, 1);
     // so far
 
-    for (int i = 0; length > i; ++i) {
+    String[] names = op.get_names();
+    String[] numbers = op.get_numbers();
+    long[] balances = op.get_balances();
+
+    for (int i = 0; op.get_user() > i; ++i) {
       System.out.printf("*** User%d の口座情報 ***\n", i);
-      System.out.printf("  口座名義: %s\n", banks[i].get_name());
-      System.out.printf("  口座番号: %s\n", banks[i].get_number());
-      System.out.printf("  預金残高: %d\n", banks[i].get_balance());
+      System.out.printf("  口座名義: %s\n", names[i]);
+      System.out.printf("  口座番号: %s\n", numbers[i]);
+      System.out.printf("  預金残高: %d\n", balances[i]);
     }
   }
 }
