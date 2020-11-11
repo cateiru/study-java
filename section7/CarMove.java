f02c5655908c81f67e79b771dd1b057050737d8d
Yuto Watanabe <yuto.w51942@gmail.com>
2020-11-11 21:01:28 +0900

    changed english to japanese.

--- section7/CarMove.java
+++ section7/CarMove.java
@@ -6,37 +6,37 @@ public class CarMove {
 
     Car myCar;
 
-    System.out.println("---- Please input spec of car ----");
-    System.out.print("* Name? : ");
+    System.out.println("---- 車のスペックを入力 ----");
+    System.out.print("* 名前? : ");
     String name = stdIn.next();
-    System.out.print("* Car Width? : ");
+    System.out.print("* 車幅? : ");
     int width = stdIn.nextInt();
-    System.out.print("* Car Height? : ");
+    System.out.print("* 車高? : ");
     int height = stdIn.nextInt();
-    System.out.print("* Car Length? : ");
+    System.out.print("* 車長? : ");
     int length = stdIn.nextInt();
-    System.out.print("* Fuel? : ");
+    System.out.print("* 燃料? : ");
     double fuel = stdIn.nextDouble();
     System.out.println();
 
     myCar = new Car(name, width, height, length, fuel);
 
-    System.out.println("---- Your Car spec ----");
+    System.out.println("---- あなたの車のスペック ----");
     myCar.putSpec();
     System.out.println();
 
     while (true) {
       putPositionFuel(myCar);
-      System.out.print("Move? 0: No, 1: Yes  :");
+      System.out.print("動かす? 0: いいえ, 1: はい  :");
       int isMove = stdIn.nextInt();
       if (isMove == 1) {
-        System.out.print("dx? :");
+        System.out.print("X座標にどれくらい? :");
         double dx = stdIn.nextDouble();
-        System.out.print("dy? :");
+        System.out.print("Y座標にどれくらい? :");
         double dy = stdIn.nextDouble();
         boolean isSuccess = myCar.move(dx, dy);
         if (!isSuccess) {
-          System.out.println("Fuel shortage!!!!");
+          System.out.println("燃料が足りない！！");
           break;
         }
       } else {
@@ -46,7 +46,7 @@ public class CarMove {
   }
 
   static void putPositionFuel(Car myCar) {
-    System.out.println("---- Current position and Fuel ----");
+    System.out.println("---- 現在の車の位置と燃料 ----");
     System.out.printf("* 位置: (%.3f, %.3f)\n", myCar.getX(), myCar.getY());
     System.out.printf("* 燃料: %.2f\n", myCar.getFuel());
     System.out.println();
