0f7e4bfa03724f4457104a7ca9f3349ba2df390e
Yuto Watanabe <yuto.w51942@gmail.com>
2020-12-09 19:47:56 +0900

    refactoring codes

--- section11/IPvol11/Client0.java
+++ section11/IPvol11/Client0.java
@@ -1,20 +1,18 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
 
 public class Client0 {
-	public static void main(String args[]) {
-		try {
-			Socket socket = new Socket(args[0],1234);
-			BufferedReader in = new BufferedReader(
-			    new InputStreamReader(socket.getInputStream()));
-			String msg = in.readLine();
-			in.close();
-			socket.close();
+  public static void main(String args[]) {
+    try {
+      Socket socket = new Socket(args[0], 1234);
+      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      String msg = in.readLine();
+      in.close();
+      socket.close();
 
-			System.out.println(msg);			
-		}
-		catch(Exception err) {
-			System.out.println(err);
-		}
-	}
+      System.out.println(msg);
+    } catch (Exception err) {
+      System.out.println(err);
+    }
+  }
 }
--- section11/IPvol11/Server0.java
+++ section11/IPvol11/Server0.java
@@ -1,24 +1,23 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
 
 public class Server0 {
-    public static final int PORT = 1234;
-	public static void main(String args[]) {
-		try {
-			ServerSocket serverSocket = new ServerSocket(PORT);
+  public static final int PORT = 1234;
 
-			while(true) {
-				Socket connect = serverSocket.accept();
+  public static void main(String args[]) {
+    try {
+      ServerSocket serverSocket = new ServerSocket(PORT);
 
-				OutputStreamWriter out =
-					new OutputStreamWriter(connect.getOutputStream());
-				out.write("You have connected to Server0 program\n");
-				out.close();
-				connect.close();
-			}
-		}
-		catch(Exception err) {
-			System.out.println(err);
-		}
-	}
+      while (true) {
+        Socket connect = serverSocket.accept();
+
+        OutputStreamWriter out = new OutputStreamWriter(connect.getOutputStream());
+        out.write("You have connected to Server0 program\n");
+        out.close();
+        connect.close();
+      }
+    } catch (Exception err) {
+      System.out.println(err);
+    }
+  }
 }
